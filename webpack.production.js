e1ea3ca85cf242095894ed82fb861390ca816a4d
Ronnie Garcia <ronnie.garcia@thecontrolgroup.com>
2019-02-06 09:51:52 -0800

    Added ability to compile with styles

--- webpack.production.js
+++ webpack.production.js
@@ -9,93 +9,103 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-	mode : 'production',
-	entry : './src/index.js',
-	output : {
-		filename : 'index.js',
-		path : path.resolve(__dirname, 'dist'),
-		publicPath : '/',
-		library : 'RemixUI',
-		libraryTarget : 'umd'
-	},
-    target : 'web',
-	resolve : {
-		extensions : ['.js', '.json', '.vue'],
-		alias : {
-			'vue$' : path.resolve(__dirname, 'node_modules/vue/dist/vue.min.js'),
-			'@' : path.resolve(__dirname, 'src')
-		}
-	},
-	externals : {
-		'vue' : 'vue',
-		'moment-timezone' : 'moment',
-		'brace' : 'brace'
-	},
-	module : {
-		rules : [
-			{
-				test : /\.js$/,
-				loader : 'babel-loader',
-				exclude : file => (/node_modules/.test(file) && !/\.vue\.js/.test(file)),
-				include : [
-					path.resolve(__dirname, 'src')
-				]
-			},
-			{
-				test : /\.css$/,
-				loader : [
-					'style-loader',
-					'css-loader',
-					'postcss-loader'
-				]
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader'
-			},
-			{
-				test : /\.less$/,
-				loader : [
-					'vue-style-loader',
-					{
-						loader : 'css-loader',
-						options : {
-							sourceMap : true
-						}
-					},
-					{
-						loader : 'postcss-loader',
-						options : {
-							sourceMap : true
-						}
-					},
-					{
-						loader : 'less-loader',
-						options : {
-							sourceMap : true
-						}
-					}
-				]
-			},
-			{
-				test : /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-				loader: 'url-loader',
-				options: {
-					limit: 10000,
-					name: '[name].[hash:7].[ext]'
-				}
+module.exports = (env) => {
+	let config = {
+		mode : 'production',
+		entry : './src/index.js',
+		output : {
+			filename : 'index.js',
+			path : path.resolve(__dirname, 'dist'),
+			publicPath : '/',
+			library : 'RemixUI',
+			libraryTarget : 'umd'
+		},
+		target : 'web',
+		resolve : {
+			extensions : ['.js', '.json', '.vue'],
+			alias : {
+				'vue$' : path.resolve(__dirname, 'node_modules/vue/dist/vue.min.js'),
+				'@' : path.resolve(__dirname, 'src')
 			}
+		},
+		externals : {
+			'vue' : 'vue',
+			'moment-timezone' : 'moment',
+			'brace' : 'brace'
+		},
+		module : {
+			rules : [
+				{
+					test : /\.js$/,
+					loader : 'babel-loader',
+					exclude : file => (/node_modules/.test(file) && !/\.vue\.js/.test(file)),
+					include : [
+						path.resolve(__dirname, 'src')
+					]
+				},
+				{
+					test : /\.css$/,
+					loader : [
+						'style-loader',
+						'css-loader',
+						'postcss-loader'
+					]
+				},
+				{
+					test: /\.vue$/,
+					loader: 'vue-loader'
+				},
+				{
+					test : /\.less$/,
+					loader : [
+						'vue-style-loader',
+						{
+							loader : 'css-loader',
+							options : {
+								sourceMap : true
+							}
+						},
+						{
+							loader : 'postcss-loader',
+							options : {
+								sourceMap : true
+							}
+						},
+						{
+							loader : 'less-loader',
+							options : {
+								sourceMap : true
+							}
+						}
+					]
+				},
+				{
+					test : /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+					loader: 'url-loader',
+					options: {
+						limit: 10000,
+						name: '[name].[hash:7].[ext]'
+					}
+				}
+			]
+		},
+		plugins : [
+			new VueLoaderPlugin(),
+			// new CleanWebpackPlugin(['dist']),
+			new LodashModuleReplacementPlugin,
+			new webpack.NamedModulesPlugin(),
+			new BundleAnalyzerPlugin({
+				analyzermode : 'static',
+				openAnalyzer : true
+			})
 		]
-	},
-	plugins : [
-		new VueLoaderPlugin(),
-		new CleanWebpackPlugin(['dist']),
-		new LodashModuleReplacementPlugin,
-		new webpack.NamedModulesPlugin(),
-		new BundleAnalyzerPlugin({
-			analyzermode : 'static',
-			openAnalyzer : true
-		})
-	]
+	};
+
+	if (env.theme) {
+		config.output.filename = 'index-styled.js';
+		config.resolve.alias['remix-ui-styles'] = path.resolve(__dirname, 'src/styles');
+		config.resolve.alias['theme'] = path.resolve(env.theme);
+	}
+
+	return config;
 };
